6ae3d3ea6482528bc2ba96eef3c40f040cf179bb
RafaelCastro23 <rafaelpuyo23@yahoo.com>
2026-07-01 15:16:48 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,43 @@
-// STUDENT NAME:
+// STUDENT NAME: Rafael Castro
 // DATE: 
 
-public class Main 
+public class JulianDayNumber 
 {
-  public static void main(String[] args) 
-	{
 
+    public static int calculateJDN(int month, int day, int year) 
+	{
+        int a = (14 - month) / 12;
+        int y = year + 4800 - a;
+        int m = month + 12 * a - 3;
+
+        return day
+                + (153 * m + 2) / 5
+                + 365 * y
+                + y / 4
+                - y / 100
+                + y / 400
+                - 32045;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter month: ");
+        int month = input.nextInt();
+
+        System.out.print("Enter day: ");
+        int day = input.nextInt();
+
+        System.out.print("Enter year: ");
+        int year = input.nextInt();
+
+        int jdn = calculateJDN(month, day, year);
+
+        System.out.println("Julian Day Number: " + jdn);
+
+        input.close();
+    }
+}
 		/***** DECLARATION SECTION *****/
 
 		/***** INITIALIZATION SECTION *****/
